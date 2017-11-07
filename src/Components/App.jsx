b98ce62825cf26b54f89bde89665b6accd606374
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-11-07 13:09:33 +0300

    feat(Login): Create Login Interface
    - add login component
    [Delivers #152631364]

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,14 +2,18 @@
  * ./Components/App.jsx
  */
 import React from 'react';
+
+// styling
 import './App.scss';
+
+// Components
+import LoginPage from '../pages/Login/LoginPage.Component';
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="content">
-                <h1 className="title">WIRE</h1>
-                <button className="button is-info">Bulma Button</button>
+                < LoginPage />
             </div>
         );
     }
