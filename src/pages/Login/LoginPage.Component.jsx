7f834887f82c4119015e1e35a19b0b5cbbabdc88
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-11-07 13:09:33 +0300

    feat(Login): Create Login Interface
    - add login component
    [Delivers #152631364]

---
+++ src/pages/Login/LoginPage.Component.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const LoginPage = () => {
+    return(
+        <div className="columns">
+            <div className="column">Login Left</div>
+            <div className="column">Login Right</div>
+        </div>
+    )
+};
+
+export default LoginPage;
