306bf8d1f9d09e643f3d8a45a6826e071334bdcf
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-11-20 17:14:05 +0300

    feat(Login): add authentication component
    update linting rules
    add PrivateRoute component
    add login and log out logic
    [Delivers #152806729]

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,20 +2,33 @@
  * ./Components/App.jsx
  */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 // styling
 import './App.scss';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div className="content">
-                <div className="column">
-                    <h1>Home</h1>
-                    <Link to="/">Logout</Link>
-                </div>
+// Components
+import { authenticateUser } from './Login/LoginPage.Component';
+
+const App = () => {
+    return (
+        <div className="content">
+            <div className="column">
+                <h1>Admin Dashboard</h1>
+                <button onClick={logOut}>Log Out</button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+/**
+ * Log out user
+ */
+const logOut = () => {
+    authenticateUser.revokeAuthentication(() => {
+        console.log('authentication revoked,', authenticateUser.isAuthenticated);
+    });
+    location.reload();
+};
+
+export default App;
--- src/pages/Login/LoginPage.Component.jsx
+++ src/pages/Login/LoginPage.Component.jsx
@@ -1,27 +1,79 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, Route, Redirect } from 'react-router-dom';
 
 //styling
 import './LoginPage.scss';
 
-const LoginPage = () => {
-    return(
-        <div className="login-page">
-            <div className="left-column">
-                <img className="logo-image" src="/assets/images/wire_logo.svg" alt="Wire Logo" />
-            </div>
-            <div className="right-column">
-                <div className="login-container">
-                    <h2 className="title">Sign in to Wire</h2>
-                    <p className="">Please use your work email</p>
-                    <a className="button">
-                        <img className="google-logo" src="/assets/images/icons8-google.svg" />
-                        <span className="">Sign in with Google</span>
-                    </a>
+class LoginPage extends React.Component{
+    constructor() {
+        super();
+
+        this.state = {
+            redirectToReferrer: false
+        };
+    }
+
+    login = () => {
+        authenticateUser.authenticate(() => {
+            this.setState({ redirectToReferrer: true });
+        });
+    }
+
+    render() {
+        const { from } = this.props.location.state || { from: { pathname: '/' } };
+        const { redirectToReferrer } = this.state;
+
+        if (redirectToReferrer) {
+            console.log('redirect?', redirectToReferrer, 'now redirect to:', from);
+            return (
+              <Redirect to={from} />
+            );
+        }
+
+        console.log('not authenticated, redirect?', redirectToReferrer);
+        return (
+            <div className="login-page">
+                <div className="left-column">
+                    <img className="logo-image" src="/assets/images/wire_logo.svg" alt="Wire Logo" />
+                </div>
+                <div className="right-column">
+                    <div className="login-container">
+                        <div>
+                            <p>You must log in to view the page at {from.pathname}</p>
+                            <button onClick={this.login}>Log in</button>
+                        </div>
+                        <h2 className="title">Sign in to Wire</h2>
+                        <p className="">Please use your work email</p>
+                        <a className="button">
+                            <img className="google-logo" src="/assets/images/icons8-google.svg" />
+                            <span className="">Sign in with Google</span>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+}
+
+// authCode
+/**
+ * authenticate user
+ * revoke authentication
+ *  on signOut
+ *      revoke authentication
+ *      redirect to landing page / login page
+ */
+export const authenticateUser = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true,
+        setTimeout(cb, 100);
+    },
+    revokeAuthentication(cb) {
+        this.isAuthenticated = false,
+        setTimeout(cb, 100);
+    }
 };
 
 export default LoginPage;
