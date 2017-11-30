b5ca680a77e48f5af004c1af9a62e02fa54dfb92
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-11-30 18:01:21 +0300

    Add material ui
    - Remove Bulma
    - Add material ui
    - refactor login page styling
    - implement PR feedback

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 
 //routes
@@ -14,7 +15,9 @@ import store from './store/store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <MuiThemeProvider>
+            <Routes />
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
 );
