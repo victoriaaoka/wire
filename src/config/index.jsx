83551b32901308b30dd0c1fa7a137f587625dcd0
Jimmy Kimani <james.kimani@andela.com>
2018-01-22 11:40:11 +0300

    #154080094 Fix Login Button (#21)
    * chore(login): Fix login button
    
    * chore(login): implement changes from review
    - change px to rem
    - add cover image height

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -3,10 +3,14 @@ const ConfigData = {
         ANDELA_API_BASE_URL : 'https://api.andela.com',
         BASE_URL: 'http://wire.andela.com:8080'
     },
+    staging: {
+        ANDELA_API_BASE_URL : 'https://api.andela.com',
+        BASE_URL: 'http://wire.now.sh'
+    },
     production: {
         ANDELA_API_BASE_URL : 'https://api.andela.com',
-        BASE_URL: 'http://wire.andela.com:8080'
-    },  
+        BASE_URL: 'http://wire.andela.com'
+    },
 };
 
 export const config = ConfigData[process.env.NODE_ENV] || ConfigData['development'];
