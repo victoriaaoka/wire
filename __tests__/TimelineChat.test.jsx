0bc04f7306bb2b4af394bf4f8409ee1c00fbb2c9
Akash <akashbaga011@gmail.com>
2018-03-08 14:02:00 +0300

    ch: Move mock_endpoints folder -correct mock endpoints folder placement -Amend tests to reflect new folder directory [Delivers #155720290]

--- __tests__/TimelineChat.test.jsx
+++ __tests__/TimelineChat.test.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import shallowToJSON from 'enzyme-to-json';
 
 import TimelineChat from '../src/Components/TimelineChat/TimelineChat.Component';
-import { testIncident } from '../src/mock_endpoints/mockData';
+import { testIncident } from '../mock_endpoints/mockData';
 
 describe('Timeline Chat component', () => {
   it('should have all the Timeline Chat content', () => {
