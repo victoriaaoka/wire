0856cb97158db6f8e92529251a1fa3a51d9188ec
peterpaints <musonyengigi@gmail.com>
2018-03-15 15:41:21 +0300

    chore: Tests for Timeline Action
    - Raises coverage to 90%
    
    [Delivers #155969467]

--- __tests__/TimelineAction.test.jsx
+++ __tests__/TimelineAction.test.jsx
@@ -1,62 +1,295 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import * as actions from '../src/actions/timelineAction';
 import * as types from '../src/actions/actionTypes';
-import { testIncident } from '../mock_endpoints/mockData';
+import { testIncident, notes, chats } from '../mock_endpoints/mockData';
+import moxios from 'moxios';
 
-describe('Actions', () => {
-  it('should create action to load incident details', () => {
-    const expectedAction = {
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('async actions', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  let updatedNote = notes[0];
+  updatedNote['note'] = 'A new note';
+
+  let incidentWithNewStatus = testIncident;
+  incidentWithNewStatus['statusId'] = 2;
+
+  let incidentWithNewAssignee = testIncident;
+  incidentWithNewAssignee['assigneeId'] = 1;
+
+  const expectedActions = [
+    {
+      type: types.IS_LOADING,
+      status: true
+    },
+    {
       type: types.FETCH_INCIDENT,
       incident: testIncident,
       isLoading: false
-    };
-    expect(actions.loadIncidentSuccess(testIncident)).toEqual(expectedAction);
-  });
-  it('should create an action to add notes', () => {
-    const note = 'This is a note';
-    const expectedAction = {
+    },
+    {
       type: types.ADD_NOTE,
-      note
-    };
-    expect(actions.addNoteSuccess(note)).toEqual(expectedAction);
-  });
-  it('should create an action to edit a note', () => {
-    const note = 'This is an edited note';
-    const expectedAction = {
+      note: notes[0]
+    },
+    {
       type: types.EDIT_NOTE,
-      note,
-      index: 1
-    };
-    expect(actions.editNoteSuccess(note, 1)).toEqual(expectedAction);
-  });
-  it('should create an action to archive a note', () => {
-    const note = 'Note to be archived';
-    const expectedAction = {
+      note: updatedNote,
+      index: 0
+    },
+    {
       type: types.ARCHIVE_NOTE,
-      note,
-      index: 1
-    };
-    expect(actions.archiveNoteSuccess(note, 1)).toEqual(expectedAction);
-  });
-  it('should create an action to change the status of an incident', () => {
-    const expectedAction = {
+      note: notes[0],
+      index: 0
+    },
+    {
       type: types.CHANGE_STATUS,
-      incident: testIncident
-    };
-    expect(actions.changeStatusSuccess(testIncident)).toEqual(expectedAction);
-  });
-  it('should create an action to change the assignee of an incident', () => {
-    const expectedAction = {
+      incident: incidentWithNewStatus
+    },
+    {
       type: types.CHANGE_ASSIGNEE,
-      incident: testIncident
-    };
-    expect(actions.changeAssigneeSuccess(testIncident)).toEqual(expectedAction);
-  });
-  it('should create an action to send a message', () => {
-    const chat = 'This is a message';
-    const expectedAction = {
+      incident: incidentWithNewAssignee
+    },
+    {
       type: types.ADD_CHAT,
-      chat
-    };
-    expect(actions.sendMessageSuccess(chat)).toEqual(expectedAction);
+      chat: chats[2]
+    },
+    { type: types.ERROR_ACTION,
+      status: true,
+      message: 'You might not be logged in/authorized. Please try again.'
+    }
+  ];
+
+  it('creates all appropriate actions when loading incident details', done => {
+    const store = mockStore();
+    store.dispatch(actions.loadIncidentDetails(testIncident.id));
+    moxios.wait(() => {
+      let incidentRequest = moxios.requests.at(0);
+      let notesRequest = moxios.requests.at(1);
+      let chatsRequest = moxios.requests.at(2);
+      incidentRequest
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: testIncident
+          }
+        });
+      notesRequest
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: {
+              notes
+            }
+          }
+        });
+      chatsRequest
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: {
+              chats
+            }
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[0]);
+          expect(storeActions[1]).toEqual(expectedActions[1]);
+          done();
+        });
+    });
+  });
+
+  it('creates all appropriate actions when adding a note', done => {
+    let noteText = notes[0].note;
+    let incidentId = notes[0].incidentId;
+    let userId = notes[0].userId;
+
+    const store = mockStore();
+    store.dispatch(actions.addNote(noteText, incidentId, userId));
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 201,
+          response: {
+            status: 'success',
+            data: notes[0]
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[2]);
+          done();
+        });
+    });
+  });
+
+  it('creates all appropriate actions when editing a note', done => {
+    let noteText = 'A new note';
+    let noteId = notes[0].id;
+    let index = 0;
+
+    let updatedNote = notes[0];
+    updatedNote['note'] = noteText;
+
+    const store = mockStore();
+    store.dispatch(actions.editNote(noteText, noteId, index));
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: updatedNote
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[3]);
+          done();
+        });
+    });
+  });
+
+  it('creates all appropriate actions when archiving a note', done => {
+    let noteId = notes[0].id;
+    let index = 0;
+
+    const store = mockStore();
+    store.dispatch(actions.archiveNote(noteId, index));
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: notes[0]
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[4]);
+          done();
+        });
+    });
+  });
+
+  it('creates all appropriate actions when changing incident status', done => {
+    let incidentId = testIncident.id;
+    let statusId = 2;
+    let incidentWithNewStatus = testIncident;
+    incidentWithNewStatus['statusId'] = 2;
+
+    const store = mockStore();
+    store.dispatch(actions.changeStatus(statusId, incidentId));
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: incidentWithNewStatus
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[5]);
+          done();
+        });
+    });
+  });
+
+  it('creates all appropriate actions when changing incident assignee', done => {
+    let incidentId = testIncident.id;
+    let assigneeId = 1;
+    let incidentWithNewAssignee = testIncident;
+    incidentWithNewAssignee['assigneeId'] = assigneeId;
+
+    const store = mockStore();
+    store.dispatch(actions.changeAssignee(assigneeId, incidentId));
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: incidentWithNewAssignee
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[6]);
+          done();
+        });
+    });
+  });
+
+  it('creates all appropriate actions when sending messages', done => {
+    let incidentId = testIncident.id;
+    let userId = 3;
+    let message = chats[2].chat;
+
+    const store = mockStore();
+    store.dispatch(actions.sendMessage(incidentId, userId, message));
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 200,
+          response: {
+            status: 'success',
+            data: chats[2]
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[7]);
+          done();
+        });
+    });
+  });
+
+  it('dispatches error action when there is an error with a request', done => {
+    let incidentId = testIncident.id;
+    let userId = 3;
+    let message = chats[2].chat;
+
+    const store = mockStore();
+    store.dispatch(actions.sendMessage(incidentId, userId, message));
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request
+        .respondWith({
+          status: 401,
+          response: {
+            status: 401,
+            data: {
+              error: 'Not authorized'
+            }
+          }
+        })
+        .then(() => {
+          const storeActions = store.getActions();
+          expect(storeActions[0]).toEqual(expectedActions[8]);
+          done();
+        });
+    });
   });
 });
