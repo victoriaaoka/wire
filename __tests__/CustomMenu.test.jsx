69de0094722fe74ba7b2dfbc6d2ebdba43f56c85
Akash <akashbaga011@gmail.com>
2018-03-23 15:41:05 +0300

    ch: Increase Test coverage for Button and Custom Menu Components
    -Adds Tests for Button Component
    -Adds test for CustomMenu Component
    
    [Delivers #156127581]

--- __tests__/CustomMenu.test.jsx
+++ __tests__/CustomMenu.test.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import shallowToJSON from 'enzyme-to-json';
 import { shallow } from 'enzyme';
-import { CustomMenu } from '../src/Components/CustomMenu/CustomMenu.Component';
+import CustomMenu from '../src/Components/CustomMenu/CustomMenu.Component';
+import SelectField from 'material-ui/SelectField';
 
 describe('CustomMenu component', () => {
   it('Should have all the CustomMenu content', () => {
     const customMenu = shallow(<CustomMenu />);
     const tree = shallowToJSON(customMenu);
     expect(tree).toMatchSnapshot();
+  });
+
+  it('Should change state when filter value changes', () => {
+    const changeCountryFilterMock = jest.fn()
+    const menu = shallow(<CustomMenu changeCountryFilter={changeCountryFilterMock}/>)
+    const select = menu.find(SelectField);
+    select.props().onChange()
+    menu.setState({value: 'Kenya'})
+    menu.update()
+    expect(menu.instance().state.value).toEqual('Kenya');
+    expect(changeCountryFilterMock.mock.calls.length).toBe(1);
   });
 });
