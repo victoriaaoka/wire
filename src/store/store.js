317329854265c664fbdf20738077928404a53433
kimobrian <brian.kimokoti@andela.com>
2018-03-31 16:48:16 +0300

    chore(devtools): Disable redux-dev tools in production

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
-  NODE_ENV !== 'production' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null,
+  process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
 );
 
